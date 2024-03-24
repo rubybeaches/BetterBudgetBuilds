43291d95966cfca584da3770e49d07d85c800035
pmclainmmllc <patrick.mclain.jr@gmail.com>
2024-03-24 13:17:28 -0500

    better handling of income field and validation

--- src/app/budget/page.tsx
+++ src/app/budget/page.tsx
@@ -18,10 +18,10 @@ const Budget = () => {
             <p>Let's generate a basic bucketing system based on national averages. Don't worry about getting it perfect the first time, you can revisit this at anytime and update your budget moving forward. We've provided percentages based on what is most recommended as a guidepost,  and you can adjust the categories and buckets to your needs. If you're unsure of what you should put in any given bucket, use your best guess or put $0 until you have a better idea.</p>
 
             <label id="income">What is your annual income after taxes and deductions?
-                <div><span>$</span> <input type="number" value={income} onChange={(e) => setIncome(Number(e.target.value))} /></div>
+                <div><span>$</span> <input type="number" value={income} onChange={(e) => setIncome(e.target.valueAsNumber)} /></div>
             </label>
 
-            <BudgetEssentials categories={categories} income={monthlyIncome} />
+            <BudgetEssentials categories={categories} income={monthlyIncome || 0} />
 
         </main>);
 }
--- src/app/components/Slider.tsx
+++ src/app/components/Slider.tsx
@@ -3,7 +3,7 @@ import { useState } from "react"
 const Slider = ({ min, max, position }: { min: number, max: number, position: number }) => {
     const [sliderPosition, setSliderPosition] = useState(position);
 
-    return <input className="slider" type="range" min={min} max={max} value={sliderPosition} onChange={(e) => setSliderPosition(Number(e.target.value))}></input>
+    return <input className="slider" type="range" min={min} max={max} value={sliderPosition} onChange={(e) => setSliderPosition(e.target.valueAsNumber)}></input>
 }
 
 export default Slider;
