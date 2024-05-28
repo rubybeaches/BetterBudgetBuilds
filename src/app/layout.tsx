0e6b84f65091abba2b6930fe21d709396d899403
pmclainmmllc <patrick.mclain.jr@gmail.com>
2024-05-28 16:26:16 -0500

    initial setup of clerk

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,14 @@
 import type { Metadata } from "next";
 import { Montserrat } from "next/font/google";
 import "./globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
 
 const Mont = Montserrat({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Better Budget Builds",
-  description: "The all in one budget application, from saving, to planning, to thriving.",
+  description:
+    "The all in one budget application, from saving, to planning, to thriving.",
 };
 
 export default function RootLayout({
@@ -15,8 +17,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={Mont.className}>{children}</body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body className={Mont.className}>{children}</body>
+      </html>
+    </ClerkProvider>
   );
 }
