1f55f7800b13a85ef2fd6d799224d11003b49d30
pmclainmmllc <patrick.mclain.jr@gmail.com>
2024-07-05 18:13:56 -0500

    adding basic function and style for recurring edit mode

--- src/app/components/AddExpense/AddExpenseRow.tsx
+++ src/app/components/AddExpense/AddExpenseRow.tsx
@@ -18,41 +18,89 @@ const AddExpenseRow = ({
     return `${year}-${month}-${day}`;
   };
 
-  return (
-    <tr>
-      <td
-        className={`expenseAmount ${recurringActive ? "recurringActive" : ""}`}
-      >
-        <span onClick={() => setRecurringActive(true)}>
-          <RecurringIcon />
-        </span>
-        ${" "}
-        <input
-          type="text"
-          name="amount"
-          value={convertToFloat(expense.amount)}
-          readOnly
-        />
-      </td>
-      <td>
-        <input
-          type="text"
-          name="description"
-          value={expense.description}
-          readOnly
-        />
-      </td>
-      <td>{expense.category}</td>
-      <td>
-        <input
-          type="date"
-          name="date"
-          value={displayDate(expense.entryDate)}
-          readOnly
-        />
-      </td>
-    </tr>
-  );
+  if (recurringEdit) {
+    return (
+      <tr>
+        <td
+          className={`expenseAmount recurringEdit ${
+            recurringActive ? "recurringActive" : ""
+          }`}
+        >
+          <span onClick={() => setRecurringEdit(false)}>
+            <RecurringIcon />
+          </span>
+
+          <div>
+            ${" "}
+            <input
+              type="text"
+              name="amount"
+              defaultValue={convertToFloat(expense.amount)}
+            />
+          </div>
+        </td>
+        <td>
+          <input
+            type="text"
+            name="description"
+            placeholder="Description of the expense you've accrued"
+            defaultValue={expense.description}
+          />
+        </td>
+        <td>{expense.category}</td>
+        <td>
+          <input
+            type="date"
+            name="date"
+            defaultValue={displayDate(expense.entryDate)}
+          />
+        </td>
+      </tr>
+    );
+  } else {
+    return (
+      <tr>
+        <td
+          className={`expenseAmount ${
+            recurringActive ? "recurringActive" : ""
+          }`}
+        >
+          <span
+            onClick={() => {
+              setRecurringEdit(true);
+              setRecurringActive(true);
+            }}
+          >
+            <RecurringIcon />
+          </span>
+          ${" "}
+          <input
+            type="text"
+            name="amount"
+            value={convertToFloat(expense.amount)}
+            readOnly
+          />
+        </td>
+        <td>
+          <input
+            type="text"
+            name="description"
+            value={expense.description}
+            readOnly
+          />
+        </td>
+        <td>{expense.category}</td>
+        <td>
+          <input
+            type="date"
+            name="date"
+            value={displayDate(expense.entryDate)}
+            readOnly
+          />
+        </td>
+      </tr>
+    );
+  }
 };
 
 export default AddExpenseRow;
